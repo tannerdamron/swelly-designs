d86c073567108e0cb51f0ff50cba2f08e8176680
Tanner Damron <tanner.mdamron@yahoo.com>
2020-02-27 14:31:24 -0800

    Updates

---
+++ precache-manifest.2bd7d6e012e9cbb27e4f8dc4675633c9.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e1371752d9b5393285e0b1216b35675",
+    "url": "swellydesigns.com/index.html"
+  },
+  {
+    "revision": "7d60a2a383e9d79ec611",
+    "url": "swellydesigns.com/static/css/2.2b603bb3.chunk.css"
+  },
+  {
+    "revision": "33cdccee228a81c3d9ba",
+    "url": "swellydesigns.com/static/css/main.7e6281be.chunk.css"
+  },
+  {
+    "revision": "7d60a2a383e9d79ec611",
+    "url": "swellydesigns.com/static/js/2.b7595314.chunk.js"
+  },
+  {
+    "revision": "33cdccee228a81c3d9ba",
+    "url": "swellydesigns.com/static/js/main.81a94def.chunk.js"
+  },
+  {
+    "revision": "9e563cd5b30ed919d900",
+    "url": "swellydesigns.com/static/js/runtime-main.e2397039.js"
+  },
+  {
+    "revision": "b1bdea561f247adc2c904f5b24a07c51",
+    "url": "swellydesigns.com/static/media/Raleway-Light.b1bdea56.ttf"
+  },
+  {
+    "revision": "229106f6405ad7c9ca99c499a02df30b",
+    "url": "swellydesigns.com/static/media/SimpleRunJumpClip2D.229106f6.gif"
+  },
+  {
+    "revision": "2365aa3022cd1f41014b2553b38c91f8",
+    "url": "swellydesigns.com/static/media/WaveBg.2365aa30.jpg"
+  },
+  {
+    "revision": "2efd38edf655cb3f183bbde5554f75a8",
+    "url": "swellydesigns.com/static/media/alleywayevents.2efd38ed.PNG"
+  },
+  {
+    "revision": "e8460f0d8b9df01e9460bfd1ade0e8c8",
+    "url": "swellydesigns.com/static/media/csharp.e8460f0d.png"
+  },
+  {
+    "revision": "a57aeaeb1cbf1b1b9adea4df1992d7ab",
+    "url": "swellydesigns.com/static/media/css.a57aeaeb.png"
+  },
+  {
+    "revision": "d027a0da2419a214ba57365c923c6ba6",
+    "url": "swellydesigns.com/static/media/dotnet.d027a0da.png"
+  },
+  {
+    "revision": "f2282f94c041c37ef4db028e428e41c1",
+    "url": "swellydesigns.com/static/media/html.f2282f94.png"
+  },
+  {
+    "revision": "35969bd4bd99f6d74101377c52e37771",
+    "url": "swellydesigns.com/static/media/joystickforums.35969bd4.PNG"
+  },
+  {
+    "revision": "94ae2dcd4fa410811cab4e1fbb403340",
+    "url": "swellydesigns.com/static/media/js.94ae2dcd.png"
+  },
+  {
+    "revision": "c2dcb3fcd57512f96b7e26a61b1849a4",
+    "url": "swellydesigns.com/static/media/jsx.c2dcb3fc.png"
+  },
+  {
+    "revision": "f36dbd0b791d17ae14ad042fbb1eba31",
+    "url": "swellydesigns.com/static/media/kninecs.f36dbd0b.PNG"
+  },
+  {
+    "revision": "f3138d9d7a2fbefa0d5e192b3e035735",
+    "url": "swellydesigns.com/static/media/primeGlass.f3138d9d.PNG"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "swellydesigns.com/static/media/react.47ce6e77.png"
+  },
+  {
+    "revision": "21829a0f16a823088ccbde8e44fb5485",
+    "url": "swellydesigns.com/static/media/reactnative.21829a0f.png"
+  },
+  {
+    "revision": "4095d6d7c9f730cb110fd137a0593d34",
+    "url": "swellydesigns.com/static/media/spaceboi.4095d6d7.PNG"
+  },
+  {
+    "revision": "b25bae44ff516c7dcc3eacdb14d2c499",
+    "url": "swellydesigns.com/static/media/sql.b25bae44.png"
+  },
+  {
+    "revision": "e0b307c2e2f78c07ac7e40a687e3d6ef",
+    "url": "swellydesigns.com/static/media/tanner.e0b307c2.png"
+  },
+  {
+    "revision": "8729db624fcc4b979e9dbd9f459daa39",
+    "url": "swellydesigns.com/static/media/tapRoom.8729db62.PNG"
+  },
+  {
+    "revision": "948483a59b4f56937328518c40854c06",
+    "url": "swellydesigns.com/static/media/unity.948483a5.png"
+  },
+  {
+    "revision": "431949c14665fe57632d056cf99074e0",
+    "url": "swellydesigns.com/static/media/unreal.431949c1.png"
+  },
+  {
+    "revision": "0539b390937e5a868f3a7176506a6118",
+    "url": "swellydesigns.com/static/media/vue.0539b390.png"
+  },
+  {
+    "revision": "fc60ddee79d436a639e5870631849f64",
+    "url": "swellydesigns.com/static/media/wave-bot.fc60ddee.png"
+  },
+  {
+    "revision": "1c24bd8de7361f3ccc0325c7b0a2d400",
+    "url": "swellydesigns.com/static/media/wave-mid.1c24bd8d.png"
+  }
+]);
