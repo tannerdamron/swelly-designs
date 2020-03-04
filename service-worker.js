3151bf932d16e8eecce54ce7308ea4fa0e3b4aca
Tanner Damron <tanner.mdamron@yahoo.com>
2020-03-04 12:47:23 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0019fd1ea0aeaa060703e43b3958b5f2.js"
+  "/precache-manifest.1bcdfee95fadc874f38457db012a9bef.js"
 );
 
 self.addEventListener('message', (event) => {
