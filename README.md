dc1cd5380c958e90df953471392e9d4160843e25
Tanner Damron <tanner.mdamron@yahoo.com>
2020-03-04 12:27:45 -0800

    remove unnecessary components, files and code

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ###### Initiated Dec 1st, 2019.
 
 ----------
-# https://swellydesigns.com/
+## [Live Site](https://swellydesigns.com/)
 
 ## Description
 This web application was built with React.
--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,16 @@
 import React, { Component } from "react";
-import { Helmet } from "react-helmet";
 import "./App.css";
 import Home from "./pages/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 import Landing from "./components/Landing/Landing";
-import cover from "./assets/cover.PNG"
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Helmet>
-          <title>Swelly Designs</title>
-          <meta
-            name="description"
-            content="Swelly Designs, a portfolio and personal business website"
-          />
-          <meta name="og:image" content={cover} />
-        </Helmet>
-        <div className="App">
-          <Landing />
-          <Navbar />
-          <Home />
-        </div>
+      <div className="App">
+        <Landing />
+        <Navbar />
+        <Home />
       </div>
     );
   }
